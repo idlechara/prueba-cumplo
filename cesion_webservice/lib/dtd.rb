5e540fdd728e813c7c3e84759d9705aa759e048d
Erik Regla <erik.regla@gmail.com>
2017-05-21 06:17:09 -0400

    realized that I messed up all my implementation. My bad, focused too much on how to solve the signing problem and didn't finished how to exchange documents.

---
+++ cesion_webservice/lib/dtd.rb
@@ -0,0 +1,60 @@
+require 'nokogiri'
+require 'signer'
+
+class DigitalTransferDocument
+  @document = nil
+  @@cert_path = '/Volumes/Yakumo/Documents/Sources/2017/cumplo/prueba/cesion_webservice/app/assets/keys/server.crt'
+  @@key_path = '/Volumes/Yakumo/Documents/Sources/2017/cumplo/prueba/cesion_webservice/app/assets/keys/server.key'
+  @@base_transfer_document = 'app/assets/schemas/schema_cesion/AEC.example.xml'
+
+  # Generates a random Cesion file with "random" parameters
+  def initialize(value, emit_date, transfer_date, deadline_tmst)
+    # Use default document at cesion_webservice/app/assets/schemas/schema_cesion/AEC.example.xml
+    document = Nokogiri::XML(File.read(@@base_transfer_document))
+
+    # Alter default values
+    document.xpath('//FchEmis').each do |tmst|
+      tmst.content = emit_date
+    end
+    document.xpath('//TmstCesion').each do |tmst|
+      tmst.content = transfer_date
+    end
+    document.xpath('//UltimoVencimiento').each do |tmst|
+      tmst.content = deadline_tmst
+    end
+    document.xpath('//MontoCesion').each do |amount|
+      amount.content = value
+    end
+    document.xpath('//MntTotal').each do |amount|
+      amount.content = value
+    end
+
+
+    ## Now, sign every part of the DTE that NEEDS to be signed.
+    sign_xml_fragment(document, '//DocumentoDTECedido/DTE' ,File.read(@@cert_path), File.read(@@key_path))
+    sign_xml_fragment(document, '//DocumentoDTECedido' ,File.read(@@cert_path), File.read(@@key_path))
+    sign_xml_fragment(document, '//Cesiones/DTECedido' ,File.read(@@cert_path), File.read(@@key_path))
+    sign_xml_fragment(document, '//Cesiones/Cesion' ,File.read(@@cert_path), File.read(@@key_path))
+    sign_xml_fragment(document, '//AEC' ,File.read(@@cert_path), File.read(@@key_path))
+
+    @document = document
+  end
+
+  ## Routine to sign a fragment of the XML.
+  def sign_xml_fragment(document, path, cert, key)
+    document.xpath(path).each do |node|
+      signer = Signer.new(node)
+      signer.cert = OpenSSL::X509::Certificate.new(cert)
+      signer.private_key = OpenSSL::PKey::RSA.new(key)
+      signer.security_node = signer.document.root
+      signer.security_token_id = ""
+      signer.digest!(signer.document.root, :id => "", :enveloped => true)
+      signer.sign!(:issuer_serial => true)
+      node.replace signer.document.to_xml
+    end
+  end
+
+  def document
+    return @document
+  end
+end
